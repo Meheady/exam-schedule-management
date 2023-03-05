82add0d2a02548ed612386ce81fbc6e42bc32b43
Meheady <hmmehedi55@gmail.com>
2023-03-06 02:26:17 +0600

    warking porduct module
    multi image view, more functionality added

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,38 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('admin.dashboard') }}">
+        <div class="sidebar-brand-icon">
+
+        </div>
+        <h5 style="font-weight: bold">Admin Panel</h5>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#moduleone">
+            <i class="fas fa-fw fa-bars fa-2x"></i>
+            <span style="font-weight: bolder;font-size: 18px">Components</span>
+        </a>
+        <div id="moduleone" class="collapse">
+            <div class="bg-white collapse-inner">
+                <a class="collapse-item " href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li>
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+<!-- End of Sidebar -->
